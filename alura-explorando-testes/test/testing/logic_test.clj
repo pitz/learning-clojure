d6775772d41b468536cb0cfc2317ade8950216f6
eduardo pitz <pitz.eduardo@gmail.com>
2021-12-13 14:58:20 -0300

    Usando LET para definir variáveis e facilitar código.

--- alura-explorando-testes/test/testing/logic_test.clj
+++ alura-explorando-testes/test/testing/logic_test.clj
@@ -22,20 +22,26 @@
     (is (not (cabe-na-fila? {:espera [1 2 3 4 5 6]} :espera)))))
 
 (deftest chega-em-com-throw-test
-  (testing "chega-em-test? adiciona primeiro elemento na fila"
-    (is (= {:espera [1]}
-           (chega-em-com-throw {:espera []} :espera 1))))
+  (let [novo-paciente     1
+        paciente-extra    6
+        hospital-vazio    {:espera []}
+        hospital-com-vaga {:espera [2 245 43 4]}
+        hospital-cheio    {:espera [2 245 43 4 novo-paciente]}
+        hospital-somente-com-novo-paciente {:espera [novo-paciente]}]
+    (testing "chega-em-test? adiciona primeiro elemento na fila"
+      (is (= hospital-somente-com-novo-paciente
+             (chega-em-com-throw hospital-vazio :espera novo-paciente))))
 
-  (testing "chega-em-test? adiciona elemento na fila"
-    (is (= {:espera [2 245 43 4 5]}
-           (chega-em-com-throw {:espera [2 245 43 4]} :espera 5))))
+    (testing "chega-em-test? adiciona elemento na fila"
+      (is (= hospital-cheio
+             (chega-em-com-throw hospital-com-vaga :espera novo-paciente))))
 
-  (testing "chega-em-test? não adiciona elemento em fila cheia"
-    (is (try
-          (chega-em-com-throw {:espera [1 2 3 4 5]} :espera 6)
-          false
-          (catch Exception e
-            (= :fila-cheia (get (ex-data e) :tipo)))))))
+    (testing "chega-em-test? não adiciona elemento em fila cheia"
+      (is (try
+            (chega-em-com-throw hospital-cheio :espera paciente-extra)
+            false
+            (catch Exception e
+              (= :fila-cheia (get (ex-data e) :tipo))))))))
 
 (deftest tenta-colocar-na-fila-test
   (testing "chega-em! adiciona primeiro elemento na fila"
